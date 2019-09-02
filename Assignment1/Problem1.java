f2addc4e20bc30bec17c34521f2c294c40d2f885
stanle-cs <54789831+stanle-cs@users.noreply.github.com>
2019-09-02 18:49:26 -0500

    Update and rename CollectNewspaperKarel.java to Problem1.java

--- Assignment1/Problem1.java
+++ Assignment1/Problem1.java
@@ -1,12 +1,6 @@
 /*
- * File: CollectNewspaperKarel.java
+ * File: Problem1.java
  * --------------------------------
- * At present, the CollectNewspaperKarel subclass does nothing.
- * Your job in the assignment is to add the necessary code to
- * instruct Karel to walk to the door of its house, pick up the
- * newspaper (represented by a beeper, of course), and then return
- * to its initial position in the upper left corner of the house.
- * 
  * In this assignment, the initial position of Karel and the location
  * of the beeper are not randomized. Hence, there is no need to
  * create a generalized algorithm for Karel. However, if we would 
