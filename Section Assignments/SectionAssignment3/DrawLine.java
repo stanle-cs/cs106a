7e184b5a94bc37ba3a1b20739ecd50f096154db3
stanle-cs <micropc-stanle-cs@users.noreply.github.com>
2019-10-03 00:03:08 -0500

    SectionAssignment3 works, preliminary codes done

---
+++ Section Assignments/SectionAssignment3/DrawLine.java
@@ -0,0 +1,50 @@
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+
+import acm.graphics.*;
+import acm.program.*;
+
+public class DrawLine extends GraphicsProgram implements MouseMotionListener, MouseListener{
+	
+	GLine line = new GLine(0,0,0,0);
+	
+	
+	int width = 300;
+	
+	int height = 300;
+	
+	boolean done = false;
+	
+	boolean reset = false;
+	
+	public void mouseClicked(MouseEvent e) {
+		if (!done) { 
+			line.setStartPoint(e.getX(), e.getY());
+			done = true;
+		}
+		else {
+			line.setEndPoint(e.getX(), e.getY());
+			done = false;
+			reset = true;
+		}
+ 
+	}
+	
+	public void mouseMoved(MouseEvent e) {
+		if (done) line.setEndPoint(e.getX(), e.getY());
+		line.setVisible(true);
+	}
+	
+	public void run() {
+		line.setVisible(false);
+		add(line);
+		GRect detectBox = new GRect(0, 0, width, height);
+		detectBox.addMouseMotionListener(this);
+		detectBox.addMouseListener(this);
+		addMouseMotionListener(this);
+		addMouseListener(this);
+		add(detectBox);
+	}
+}
---
+++ Section Assignments/SectionAssignment3/RandomCircles.java
@@ -0,0 +1,35 @@
+import acm.program.*;
+import acm.graphics.*;
+import acm.util.*;
+
+import java.awt.Color;
+
+public class RandomCircles extends GraphicsProgram{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	RandomGenerator rgen = RandomGenerator.getInstance();
+	public static final double WIDTH = 100;
+	public static final double HEIGHT = 100;
+	
+	public void run() {
+		
+		for (int i = 1; i < 10; i++) {
+			
+			double randd = rgen.nextDouble(5.0, 50.0);
+			double randx = rgen.nextDouble(randd / 2, WIDTH - randd / 2);
+			double randy = rgen.nextDouble(randd / 2, HEIGHT - randd / 2);
+			Color color = rgen.nextColor();
+			
+			add( new GRect(0, 0, WIDTH, HEIGHT));
+		
+			
+			GOval circle = new GOval(randx, randy, randd / 2, randd / 2);
+			circle.setColor(color);
+			circle.setFilled(true);
+			add(circle);
+		}
+	}
+}
